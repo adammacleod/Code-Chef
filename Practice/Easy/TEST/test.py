f9ccf88d0fb4bac079c40ed1a55272d42b30b796
Adam MacLeod <adam.macleod@gmail.com>
2012-01-29 22:59:20 +1100

    Completed TEST

---
+++ Practice/Easy/TEST/test.py
@@ -0,0 +1,14 @@
+"""
+CodeChef - Life, the Universe, and Everything
+
+Problem Code: TEST
+URL: http://www.codechef.com/problems/TEST
+Author: Adam MacLeod <adam.macleod@gmail.com>
+Created: 29/01/2012
+"""
+
+while True:
+    input = int(raw_input())
+    if input is 42: 
+        break 
+    print input
